10c1e17dfa8b0d41c57baa952ead833f878129f2
unknown <pallabjyotisingha@gmail.com>
2021-12-20 04:48:34 +0530

    react app initialized

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 style={{ textAlign: "center" }}>
+                    Hello! from Thengs community
+                    <br />
+                    We are currently working on our product
+                    <br /> please visit us later, Thanks.
+                </h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
