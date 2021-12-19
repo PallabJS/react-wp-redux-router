ecb945687639dfc072434a63a79454515ea8b9c6
unknown <pallabjyotisingha@gmail.com>
2021-12-20 05:02:18 +0530

    gh action 1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
                     Hello! from Thengs community
                     <br />
                     We are currently working on our product
-                    <br /> please visit us later, Thanks.
+                    <br /> please visit us later, Thanks!
                 </h1>
             </header>
         </div>
